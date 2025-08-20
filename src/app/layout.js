3b1ea7e3a2f24dc616c706d74479f3e255927adf
capitalhealthcaree <premierpatientappointment@gmail.com>
2025-08-20 15:07:13 +0500

    add no index tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,24 @@
 'use client';
 import { useEffect } from 'react';
 import { Providers } from '@/store/provider';
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "@/styles/style.scss";
 
-const  RootLayout = ({ children }) => {
+const RootLayout = ({ children }) => {
 
 	useEffect(() => {
 		window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.min.js");
 	}, []);
-	
-	return ( 
+
+	return (
 		<html lang="en">
 			<head>
-			<link rel="stylesheet" href="/css/font-awesome.css" />
-			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" />
-			<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
+				<meta name="robots" content="noindex, nofollow" />
+				<link rel="stylesheet" href="/css/font-awesome.css" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
 			</head>
 			<body>
 				<Providers>
@@ -25,8 +26,8 @@ const  RootLayout = ({ children }) => {
 				</Providers>
 			</body>
 		</html>
-	 );
+	);
 }
- 
+
 export default RootLayout;
 
